fb0f0181d5b781b9b7dee2635089f61121e5bbe4
Reilly Moore <williammoore6105@gmail.com>
2020-07-09 22:56:03 -0500

    Suggestions courtesy Yardanico <3

--- src/jeknil.nim
+++ src/jeknil.nim
@@ -1,5 +1,4 @@
-import os, parseopt, parsecfg, strutils, streams, times, re, markdown, text,
-  strformat
+import os, parseopt, parsecfg, strutils, streams, times, re, markdown, text
 
 # current date, no more constant checking
 let date = getDateStr()
@@ -14,12 +13,12 @@ proc getMetadata(f: string): Config =
   result = loadConfig(newStringStream(f))
 
 # replace certain characters for a friendlier title
-template makeTitle(s: string): string =
+proc makeTitle(s: string): string =
   s.multiReplace({" ": "-", ".": "", "'": ""})
     .toLowerAscii() & '_' & date & ".html"
 
 # shorthand for replacing vars with values
-template replaceVars(s, t, d, c: string): string =
+proc replaceVars(s, t, d, c: string): string =
   s.multiReplace({
     "{content}": c, 
     "{title}": t, 
@@ -35,104 +34,108 @@ proc tryLoadTemplate(t: string): string =
     quit "Unable to find template.html. Aborting.", QuitFailure
 
 
-if paramCount() > 0:
-  var
-    # default values
-    files: seq[string]
-    outputLocation = getCurrentDir().addSlash()
-    templateLocation = getCurrentDir().addSlash()
-    generateExample = false
-    generatePlain = false
-
-  block ParseOptions:
-    # get options and adjust above vars accordingly
-    var opt = initOptParser(commandLineParams())
-    opt.next()
-    while opt.kind != cmdEnd:
-      case opt.kind
-      # add files to list
-      of cmdArgument: files.add(opt.key)
-      of cmdLongOption, cmdShortOption:
-        case opt.key
-        of "t", "template": templateLocation = opt.val.addSlash()
-        of "o", "output": outputLocation = opt.val.addSlash()
-        of "e", "example": generateExample = true
-        of "p", "plain": generatePlain = true
-        else: discard # ignore unknown flags
-      of cmdEnd: discard # unreachable state
-      opt.next()
-
-
-  if generateExample: files = @["Template Example"]
-  # make sure there's at least 1 file
-  if files.len == 0: quit help_message, QuitSuccess
-  
-
-  # set template
-  let jtemplate =
-    if generatePlain: plain_template
-    else: tryLoadTemplate(templateLocation)
-
-
-  for file in files:
-    # create empty values
+proc main() =
+  if paramCount() > 0:
     var
-      title: string
-      description: string
-      content: string
+      # default values
+      files: seq[string]
+      outputLocation = getCurrentDir().addSlash()
+      templateLocation = getCurrentDir().addSlash()
+      generateExample = false
+      generatePlain = false
 
-    # set defaults if applicable
-    if generateExample:
-      title = "Template Example"
-      description = "This is an example post for testing Jeknil templates."
-      content = example_template.markdown()
-    # otherwise find them in the file
-    else:
-      let
-        # load the file
-        post = readFile(file)
-      # regex for finding header, depends on which comes first
-      var htmlheader: bool
-      block CheckHeader:
-        let (a, b) = (post.find("<!--").abs(), post.find("~!").abs())
-        if a == b:
-          quit "File " & file & " appears to be missing a Header. Aborting.",
-            QuitFailure
-        htmlheader = a < b
-      let
-        MetaRe = 
-          if htmlheader: re"(?s)<!--.+-->"
-          else: re"(?s)(~!).+(!~)"
-        # "size" of header start and end
-        (s, e) =
-          if htmlheader: (4, 3)
-          else: (2, 2)
-        # find start and end of header
-        loc = post.findBounds(MetaRe)
-        # use slice to ignore start and end, strip potential whitespace, parse
-        metadata = getMetadata(post[loc.first+s..loc.last-e].strip())
-      title = metadata.getSectionValue("", "title")
-      description = metadata.getSectionValue("", "description")
-      # skip header completely, strip just in case, turn into html
-      content = post[loc.last+1..^1].strip().markdown()
-    
-    
-    # check if any fields are empty
-    if title == "" or description == "" or content == "":
-      quit "File " & file & " appears to be missing one or more required " &
-        "elements (Title, Description, Content). Aborting.", QuitFailure
+    block ParseOptions:
+      # get options and adjust above vars accordingly
+      var opt = initOptParser(commandLineParams())
+      opt.next()
+      while opt.kind != cmdEnd:
+        case opt.kind
+        # add files to list
+        of cmdArgument: files.add(opt.key)
+        of cmdLongOption, cmdShortOption:
+          case opt.key
+          of "t", "template": templateLocation = opt.val.addSlash()
+          of "o", "output": outputLocation = opt.val.addSlash()
+          of "e", "example": generateExample = true
+          of "p", "plain": generatePlain = true
+          else: discard # ignore unknown flags
+        of cmdEnd: discard # unreachable state
+        opt.next()
+
+
+    if generateExample: files = @["Template Example"]
+    # make sure there's at least 1 file
+    if files.len == 0: quit help_message, QuitSuccess
     
 
-    stdout.write("Writing " & file & "... ")
-    # set dir and title, run the replacement
-    writeFile(outputLocation & title.makeTitle(),
-              jtemplate.replaceVars(title, description, content))
-    stdout.write("Done!\n")
+    # set template
+    let jtemplate =
+      if generatePlain: plain_template
+      else: tryLoadTemplate(templateLocation)
 
 
-  quit "  All finished!", QuitSuccess
+    for file in files:
+      # create empty values
+      var
+        title: string
+        description: string
+        content: string
+
+      # set defaults if applicable
+      if generateExample:
+        title = "Template Example"
+        description = "This is an example post for testing Jeknil templates."
+        content = example_template.markdown()
+      # otherwise find them in the file
+      else:
+        let
+          # load the file
+          post = readFile(file)
+        # regex for finding header, depends on which comes first
+        var htmlheader: bool
+        block CheckHeader:
+          let (a, b) = (post.find("<!--").abs(), post.find("~!").abs())
+          if a == b:
+            quit "File " & file & " appears to be missing a Header. Aborting.",
+              QuitFailure
+          htmlheader = a < b
+        let
+          MetaRe = 
+            if htmlheader: re"(?s)<!--.+-->"
+            else: re"(?s)(~!).+(!~)"
+          # "size" of header start and end
+          (s, e) =
+            if htmlheader: (4, 3)
+            else: (2, 2)
+          # find start and end of header
+          loc = post.findBounds(MetaRe)
+          # use slice to ignore start and end, strip potential whitespace, parse
+          metadata = getMetadata(post[loc.first+s..loc.last-e].strip())
+        title = metadata.getSectionValue("", "title")
+        description = metadata.getSectionValue("", "description")
+        # skip header completely, strip just in case, turn into html
+        content = post[loc.last+1..^1].strip().markdown()
+      
+      
+      # check if any fields are empty
+      if title == "" or description == "" or content == "":
+        quit "File " & file & " appears to be missing one or more required " &
+          "elements (Title, Description, Content). Aborting.", QuitFailure
+      
+
+      stdout.write("Writing " & file & "... ")
+      # set dir and title, run the replacement
+      writeFile(outputLocation & title.makeTitle(),
+                jtemplate.replaceVars(title, description, content))
+      stdout.write("Done!\n")
 
 
+    quit "  All finished!", QuitSuccess
 
-else:
-  quit help_message, QuitSuccess
+
+  else:
+    quit help_message, QuitSuccess
+
+
+when isMainModule:
+  main()
